2d9dab772635038712f058ccfc0d1bf7bb7199f6
Alex K <64101220+ak4153@users.noreply.github.com>
2021-09-23 19:15:12 +0300

    Delete models directory

--- models/Todo.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-const todoScehma = new mongoose.Schema({
-  todosArray: {},
-});
-
-const todo = mongoose.model("Todos1", todoScehma);
-
-module.exports = todo;
